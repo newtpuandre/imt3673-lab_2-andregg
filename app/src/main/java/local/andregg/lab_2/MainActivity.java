855ce7cd7dcdacb6811805af7ada0b5bed419a5d
newtpuandre <andgun96@gmail.com>
2019-03-04 14:46:24 +0100

    Intial work

--- app/src/main/java/local/andregg/lab_2/MainActivity.java
+++ app/src/main/java/local/andregg/lab_2/MainActivity.java
@@ -1,13 +1,50 @@
 package local.andregg.lab_2;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements RecyclerViewAdapter.ItemClickListener{
+
+    RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Variables
+        final Button btnSettings = findViewById(R.id.settings_button);
+
+        //Dummy data
+        ArrayList<String> testData = new ArrayList<>();
+        testData.add("Nothing here yet... Please add a RSS / ATOM 2.0 feed in the settings tab.");
+
+
+        //Set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerViewAdapter(this, testData);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+        //Settings button logic
+        btnSettings.setOnClickListener(v -> {
+            Intent I = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(I);
+        });
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "POG", Toast.LENGTH_SHORT).show();
     }
 }

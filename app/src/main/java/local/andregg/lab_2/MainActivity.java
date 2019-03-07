67e9a4c0ca680b553fc548a89606929226261cb3
newtpuandre <andgun96@gmail.com>
2019-03-07 12:58:47 +0100

    removed some stuff

--- app/src/main/java/local/andregg/lab_2/MainActivity.java
+++ app/src/main/java/local/andregg/lab_2/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 
